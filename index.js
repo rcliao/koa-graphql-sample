16d0006adf6de9e8ba31476c3478262dcde78ebd
Eric Liao <rcliao01@gmail.com>
2017-02-25 12:45:07 -0800

    feat: object types tutorial

--- index.js
+++ index.js
@@ -4,13 +4,38 @@ const mount = require('koa-mount');
 const graphHttp = require('koa-graphql');
 
 const schema = buildSchema(`
+	type RandomDice {
+		numSides: Int!
+		rollOnce: Int!
+		roll(numRolls: Int!): [Int]
+	}
 	type Query {
 		hello: String
 		quoteOfTheDay: String
 		random: Float!
 		rollThreeDice: [Int]
+		rollDice(numDices: Int!, numSides: Int): [Int],
+		getDice(numSides: Int): RandomDice
 	}
 `);
+
+class RandomDice {
+	constructor (numSides) {
+		this.numSides = numSides;
+	}
+
+	rollOnce () {
+		return 1 + Math.floor(Math.random() * this.numSides);
+	}
+
+	roll ({numRolls}) {
+		let output = [];
+		for (let i = 0; i < numRolls; i++) {
+			output.push(this.rollOnce());
+		}
+		return output;
+	}
+}
 
 const root = {
 	hello: () => 'Hello World!',
@@ -22,6 +47,16 @@ const root = {
 	},
 	rollThreeDice: () => {
 		return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+	},
+	rollDice: ({numDices, numSides}) => {
+		let output = [];
+		for (var i = 0; i < numDices; i ++) {
+			output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+		}
+		return output;
+	},
+	getDice: ({numSides}) => {
+		return new RandomDice(numSides || 6);
 	}
 };
 
